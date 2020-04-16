03a94b9d1418b76437e4c50c2e77122198155e02
Marta Ferri <marta.ferri.p@gmail.com>
2020-04-16 11:21:05 +0200

    Prepare endpoints to be implemented

--- src/beacon_api/endpoints/__init__.py
+++ src/beacon_api/endpoints/__init__.py
@@ -3,36 +3,52 @@
 from aiohttp import web
 
 from . import (info,
-               query,
-               individuals,
                filtering_terms,
                access_levels,
-               test,
-               marta)
+            #    schemas,
+               query,
+            #    genomic_query,
+               individuals,
+            #    biosamples,
+            #    gvariant,
+               )
 
 routes = [
-    web.get('/test'        , test.test),
-
     # Info
-    web.get('/'            , info.handler_root),
-    web.get('/info'        , info.handler_info),
-    web.get('/service-info', info.handler_service_info),
+    web.get('/'                 , info.handler_root),
+    web.get('/info'             , info.handler_info),
+    web.get('/service-info'     , info.handler_service_info),
+
+    # Filtering terms
+    web.get('/filtering_terms'  , filtering_terms.handler),
+
+    # Access levels
+    web.get('/access_levels'    , access_levels.handler),
+
+    # Schemas
+    # web.get('/schemas'          , schemas.handler),
 
     # Query
-    web.get('/query'       , query.handler),
+    web.get('/query'                            , query.handler),
+
+    # Genomic query
+    # web.get('/genomic_snp'                      , genomic_query.handler)
+    # web.get('/genomic_region'                   , genomic_query.handler)
 
     # Individuals
-    web.get('/individuals_rest', individuals.handler),
-    web.get('/individuals_rest/{target_id_req}', individuals.handler),
-    web.post('/individuals_rest', individuals.handler),
-    web.post('/individuals_rest/{target_id_req}', individuals.handler),
+    web.get('/individuals'                 , individuals.handler),
+    web.get('/individuals/{target_id_req}' , individuals.handler),
+    web.post('/individuals'                , individuals.handler),
+    web.post('/individuals/{target_id_req}', individuals.handler),
 
-    # Filtering terms
-    web.get('/filtering_terms', filtering_terms.handler),
+    # Biosamples
+    # web.get('/biosamples'                  , biosamples.handler),
+    # web.get('/biosamples/{target_id_req}'  , biosamples.handler),
+    # web.post('/biosamples'                 , biosamples.handler),
+    # web.post('/biosamples/{target_id_req}' , biosamples.handler),
+    
+    # gvariant
+    # web.get('/gvariant'                      , gvariant.handler)
 
-    # Filtering terms
-    web.get('/access_levels', access_levels.handler),
-
-    web.get('/marta', marta.handler),
 ]
 
