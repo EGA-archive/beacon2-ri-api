b6b4b7c994c2163c31896e256f03037b7c3d493a
Marta Ferri <marta.ferri.p@gmail.com>
2020-06-17 10:58:06 +0200

    Fix bug in service-info model

--- src/beacon_api/utils/models.py
+++ src/beacon_api/utils/models.py
@@ -6,7 +6,7 @@ import ast
 
 from .. import __id__, __beacon_name__, __apiVersion__, __org_id__, __org_name__, __org_description__, __org_adress__, __org_welcomeUrl__, __org_contactUrl__, __org_logoUrl__, __org_info__
 from .. import __description__, __version__, __welcomeUrl__, __alternativeUrl__, __createDateTime__, __updateDateTime__
-from .. import __service__, __serviceUrl__, __entryPoint__, __open__, __service_type__, __documentationUrl__, __environment__
+from .. import __service__, __serviceUrl__, __entryPoint__, __open__, __service_type__, __documentationUrl__, __environment__, __type_group__, __type_artifact__, __type_version__
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -92,7 +92,11 @@ def GA4GH_ServiceInfo_v01(host):
     GA4GH_ServiceInfo_v01 = {
         'id': __id__,
         'name': __beacon_name__,
-        'type': __service_type__,
+        'type': {
+            'group': __type_group__,
+            'artifact': __type_artifact__,
+            'version': __type_version__
+        },
         'description': __description__,
         "organization": {'name': __org_name__,
                         'url': __org_welcomeUrl__},
