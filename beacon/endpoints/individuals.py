974ca309577e7e8bd84bcc2919ee715bce15d3ce
Sabela <sabela.delatorre@crg.eu>
2020-08-31 13:32:13 +0200

    Add missing return keyword

--- beacon/endpoints/individuals.py
+++ beacon/endpoints/individuals.py
@@ -22,27 +22,19 @@ individual_gvariant_proxy = GVariantParameters()
 async def handler_gvariants(request):
 
     LOG.info('Running a gvariant by individual request')
-    await generic_individual_handler(request, fetch_variants, build_variant_response)
-    # _, qparams_db = await individual_gvariant_proxy.fetch(request)
-    #
-    # if LOG.isEnabledFor(logging.DEBUG):
-    #     print_qparams(qparams_db, individual_gvariant_proxy, LOG)
-    #
-    # response = fetch_variants(ind=qparams_db.targetIdReq)
-    #
-    # return await prepare_response(qparams_db, request, response, build_variant_response, qparams_db.targetIdReq)
+    return await generic_individual_handler(request, fetch_variants, build_variant_response)
 
 
 async def handler_biosamples(request):
 
     LOG.info('Running a biosamples by individual request')
-    await generic_individual_handler(request, fetch_biosamples, build_biosample_response)
+    return await generic_individual_handler(request, fetch_biosamples, build_biosample_response)
 
 
 async def handler_individuals(request):
 
     LOG.info('Running an individual request')
-    await generic_individual_handler(request, fetch_individuals, build_individual_response)
+    return await generic_individual_handler(request, fetch_individuals, build_individual_response)
 
 
 async def generic_individual_handler(request, fetch_function, build_response_type):
