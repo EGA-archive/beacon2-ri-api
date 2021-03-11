0da936a778fc80f0793d73d7da16f8652de6544b
Roberto <rob.ariosa@hotmail.com>
2021-03-11 11:06:36 +0100

    Update supported schemas names

--- beacon/endpoints/rest/schemas/__init__.py
+++ beacon/endpoints/rest/schemas/__init__.py
@@ -5,13 +5,13 @@ from . import default, alternative
 
 supported_schemas = {
     # default
-    'beacon-info-v2.0.0-draft.2': default.beacon_info_v30,
-    'beacon-dataset-v2.0.0-draft.2': default.beacon_dataset_info_v30,
-    'beacon-variant-v2.0.0-draft.2': default.beacon_variant_v30,
-    'beacon-variant-annotation-v2.0.0-draft.2': default.beacon_variant_annotation_v30,
-    'beacon-biosample-v2.0.0-draft.2': default.beacon_biosample_v30,
-    'beacon-individual-v2.0.0-draft.2': default.beacon_individual_v30,
-    'beacon-cohort-draft3.1': default.beacon_cohort_v31,
+    'beacon-info-v2.0.0-draft.3': default.beacon_info_v30,
+    'beacon-dataset-v2.0.0-draft.3': default.beacon_dataset_info_v30,
+    'beacon-variant-v2.0.0-draft.3': default.beacon_variant_v30,
+    'beacon-variant-annotation-v2.0.0-draft.3': default.beacon_variant_annotation_v30,
+    'beacon-biosample-v2.0.0-draft.3': default.beacon_biosample_v30,
+    'beacon-individual-v2.0.0-draft.3': default.beacon_individual_v30,
+    'beacon-cohort-v2.0.0-draft3': default.beacon_cohort_v31,
     # alternative
     'ga4gh-service-info-v1.0': alternative.ga4gh_service_info_v10,
     # phenopackets format
