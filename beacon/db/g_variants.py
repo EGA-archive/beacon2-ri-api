e13cfee1d239035e609a31f42ebfaef3c4619fe0
Roberto <rob.ariosa@hotmail.com>
2021-12-09 15:03:51 +0100

    Fix requestParameters

--- beacon/db/g_variants.py
+++ beacon/db/g_variants.py
@@ -1,7 +1,7 @@
-from beacon.db.filters import apply_filters
+from beacon.db.filters import apply_alphanumeric_filter, apply_filters
 from beacon.db.schemas import DefaultSchemas
 from beacon.db.utils import query_id, query_ids
-from beacon.request.model import RequestParams
+from beacon.request.model import AlphanumericFilter, RequestParams
 from beacon.db import client
 
 VARIANTS_PROPERTY_MAP = {
@@ -19,9 +19,17 @@ VARIANTS_PROPERTY_MAP = {
     "aachange": "molecularAttributes.aminoacidChanges"
 }
 
+def apply_request_parameters(query: dict, qparams: RequestParams):
+    for k, v in qparams.query.request_parameters.items():
+        query = apply_alphanumeric_filter(query, AlphanumericFilter(
+            id=VARIANTS_PROPERTY_MAP[k],
+            value=v
+        ))
+    return query
 
 def get_variants(entry_id: str, qparams: RequestParams):
-    query = apply_filters({}, qparams.query.filters)
+    query = apply_request_parameters({}, qparams)
+    query = apply_filters(query, qparams.query.filters)
     return DefaultSchemas.GENOMICVARIATIONS, client.beacon.genomicVariations \
         .find(query) \
         .skip(qparams.query.pagination.skip) \
@@ -29,7 +37,8 @@ def get_variants(entry_id: str, qparams: RequestParams):
 
 
 def get_variant_with_id(entry_id: str, qparams: RequestParams):
-    query = apply_filters({}, qparams.query.filters)
+    query = apply_request_parameters({}, qparams)
+    query = apply_filters(query, qparams.query.filters)
     query = query_id(query, entry_id)
     return DefaultSchemas.GENOMICVARIATIONS, client.beacon.genomicVariations \
         .find(query) \
@@ -38,12 +47,14 @@ def get_variant_with_id(entry_id: str, qparams: RequestParams):
 
 
 def get_biosamples_of_variant(entry_id: str, qparams: RequestParams):
-    query = apply_filters({}, qparams.query.filters)
+    query = apply_request_parameters({}, qparams)
+    query = apply_filters(query, qparams.query.filters)
     query = query_id(query, entry_id)
     ids = client.beacon.genomicVariations \
         .find_one(query, {"biosamplesIds": 1})
 
-    query = query_ids({}, ids)
+    query = apply_request_parameters({}, qparams)
+    query = query_ids(query, ids)
     return DefaultSchemas.BIOSAMPLES, client.beacon.biosamples \
         .find(query) \
         .skip(qparams.query.pagination.skip) \
@@ -52,6 +63,7 @@ def get_biosamples_of_variant(entry_id: str, qparams: RequestParams):
 
 def get_individuals_of_variant(entry_id: str, qparams: RequestParams):
     query = {"variantIds": entry_id}
+    query = apply_request_parameters(query, qparams)
     return DefaultSchemas.INDIVIDUALS, client.beacon.individuals \
         .find(query) \
         .skip(qparams.query.pagination.skip) \
--- beacon/request/__init__.py
+++ beacon/request/__init__.py
@@ -1,11 +1,5 @@
-import json
-from dataclasses import dataclass
-from enum import Enum
-from typing import List
-from dataclasses_json import dataclass_json, LetterCase
 from aiohttp.web_request import Request
-from beacon.request.model import IncludeResultsetResponses, RequestParams, RequestMeta, RequestQuery, Pagination
-from beacon import conf
+from beacon.request.model import IncludeResultsetResponses, RequestParams
 
 import logging
 
@@ -18,15 +12,17 @@ async def get_parameters(request: Request) -> RequestParams:
         LOG.debug(params)
         return params
     else:
-        requested_schema: str = request.query.get("requestedSchema", "")
-        skip: int = int(request.query.get("skip", 0))
-        limit: int = int(request.query.get("limit", 10))
-        include_resultset_responses: IncludeResultsetResponses = \
-            IncludeResultsetResponses(request.query.get("includeResultsetResponses", "HIT"))
-        return RequestParams(
-            RequestMeta([requested_schema] if requested_schema else []),
-            RequestQuery(
-                include_resultset_responses=include_resultset_responses,
-                pagination=Pagination(skip, limit)
-            )
-        )
+        params = RequestParams()
+        for k, v in request.query.items():
+            if k == "requestedSchema":
+                params.meta.requested_schemas = [v]
+            elif k == "skip":
+                params.query.pagination.skip = int(v)
+            elif k == "limit":
+                params.query.pagination.limit = int(v)
+            elif k == "includeResultsetResponses":
+                params.query.include_resultset_responses = IncludeResultsetResponses(v)
+            else:
+                params.query.request_parameters[k] = v
+        LOG.debug(params)
+        return params
