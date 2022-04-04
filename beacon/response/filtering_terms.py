5f8616ee1fc3502b0e6337214516bc4b33d72c78
Roberto <rob.ariosa@hotmail.com>
2022-04-04 14:11:34 +0200

    Fix filtering terms

--- beacon/response/filtering_terms.py
+++ beacon/response/filtering_terms.py
@@ -14,12 +14,13 @@ from beacon.utils.stream import json_stream
 
 
 async def handler(request, qparams: RequestParams, entity_schema: DefaultSchemas):
+    _, _, docs = get_filtering_terms(entry_id=None, qparams=qparams)
     ontology_terms = [
         {
             'id': record['ontology'] + ':' + record['term'],
             'label': record['label']
         }
-        async for record in get_filtering_terms()
+        async for record in docs
     ]
     response = {
         'meta': conf.beacon_id,
