538f7b56dee5cd83856bfc93273b04885715987b
Roberto <rob.ariosa@hotmail.com>
2022-04-11 17:38:47 +0200

    Add streaming support for Cursor

--- beacon/utils/json.py
+++ beacon/utils/json.py
@@ -3,14 +3,21 @@ import logging
 from dataclasses import is_dataclass
 from decimal import Decimal
 from json.encoder import py_encode_basestring_ascii
+from bson.objectid import ObjectId
 
 from asyncpg import Record
 
 from json import loads as parse_json
 
+from pymongo.cursor import Cursor
+
 LOG = logging.getLogger(__name__)
 
 _INFINITY = float('inf')
+
+
+def is_cursor(o):
+    return isinstance(o, Cursor)
 
 
 def is_list(o):
@@ -76,6 +83,8 @@ def _atom(o):
             return float.__repr__(o)
     elif isinstance(o, Decimal):
         return str(o)  # keeps the decimals, float would truncate them
+    elif isinstance(o, ObjectId):
+        return py_encode_basestring_ascii(str(o))
     # not a common type
     return None
 
@@ -92,6 +101,9 @@ async def _compound(o, circulars):
             yield i
     elif is_dataclass(o):
         async for i in _iterencode_dataclass(o, circulars):
+            yield i
+    elif is_cursor(o):
+        async for i in _iterencode_cursor(o, circulars):
             yield i
     else:
         raise TypeError(f'Unsupported type: {o.__class__.__name__}')
@@ -152,6 +164,33 @@ async def _iterencode_async_gen(g, circulars):
             yield _ITEM_SEPARATOR
         async for i in _iterencode(item, circulars):
             yield i
+    yield ']'
+    del circulars[marker]
+
+
+async def _iterencode_cursor(g, circulars):
+    yield '['
+    marker = id(g)
+    if marker in circulars:
+        raise ValueError("Circular reference detected")
+    circulars[marker] = g
+    first = True
+
+    try:
+        item = g.next()
+    except:
+        item = None
+    while item:
+        if first:
+            first = False
+        else:
+            yield _ITEM_SEPARATOR
+        async for i in _iterencode(item, circulars):
+            yield i
+        try:
+            item = g.next()
+        except:
+            item = None
     yield ']'
     del circulars[marker]
 
