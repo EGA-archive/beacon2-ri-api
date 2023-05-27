593f52fa388618aaca5b7e52d171bbd88a5ec1e1
Oriol López-Doriga <orildsaga@gmail.com>
2023-05-27 16:24:40 +0200

    tests 2

--- beacon/db/extract_filtering_terms.py
+++ beacon/db/extract_filtering_terms.py
@@ -194,8 +194,8 @@ def find_ontology_terms_used(collection_name: str) -> List[Dict]:
     terms_ids = []
     count = client.beacon.get_collection(collection_name).estimated_document_count()
 
-    xs = client.beacon.get_collection(collection_name).find().limit(10000)
-    for r in tqdm(xs, total=10000):
+    xs = client.beacon.get_collection(collection_name).find().limit(100000)
+    for r in tqdm(xs, total=100000):
         matches = ONTOLOGY_REGEX.findall(str(r))
         for ontology_id, term_id in matches:
             term = ':'.join([ontology_id, term_id])
