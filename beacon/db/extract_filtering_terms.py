b75bc15c3ecda600de439ccdc4c7d45a5d93a305
Oriol López-Doriga <orildsaga@gmail.com>
2023-05-27 13:15:14 +0200

    filtering objects

--- beacon/db/extract_filtering_terms.py
+++ beacon/db/extract_filtering_terms.py
@@ -182,9 +182,20 @@ def insert_all_ontology_terms_used():
     print("Collections:", collections)
     for c_name in collections:
         terms_ids = find_ontology_terms_used(c_name)
-        terms = get_filtering_object(terms_ids, c_name)
-        if len(terms) > 0:
-            client.beacon.filtering_terms.insert_many(terms)
+        i = 0
+        j =50
+        while i < len(terms_ids)-1:
+            if j < len(terms_ids)-1:
+                terms_partials = terms_ids[i:j]
+                i+=50
+                j+=50
+            else:
+                terms_partials = terms_ids[i:-1]
+                i+=50
+                j+=50
+            terms = get_filtering_object(terms_partials, c_name)
+            if len(terms) > 0:
+                client.beacon.filtering_terms.insert_many(terms)
 
 def find_ontology_terms_used(collection_name: str) -> List[Dict]:
     print(collection_name)
