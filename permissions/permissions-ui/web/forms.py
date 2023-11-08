d981351dd66bc184807b287df8731fc793879527
Oriol López-Doriga <orildsaga@gmail.com>
2023-11-08 14:18:39 +0100

    minor fixes

--- permissions/permissions-ui/web/forms.py
+++ permissions/permissions-ui/web/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class BamForm(forms.Form):
-    choices = [("PUBLIC", "PUBLIC"), ("REGISTERED", "REGISTERED"), ("CONTROLLED", "CONTROLLED")]    
+    choices = [("PUBLIC", "PUBLIC"), ("CONTROLLED", "CONTROLLED"), ("REGISTERED", "REGISTERED")]    
     Datasets = forms.ChoiceField(choices=choices, help_text="<span class='hovertext' data-hover='Choose what type of datasets you want to manage'>Datasets type:</span>", label="")
