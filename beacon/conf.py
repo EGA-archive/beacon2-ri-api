4ae748b59a8f7c60fbf707decff0b5bf31769765
Oriol López-Doriga <orildsaga@gmail.com>
2023-11-27 23:53:52 +0100

    beacon conf uri addressed

--- beacon/conf.py
+++ beacon/conf.py
@@ -12,7 +12,7 @@ import os
 beacon_id = 'org.ega-archive.ga4gh-approval-beacon-test'  # ID of the Beacon
 beacon_name = 'GA4GH Approval Beacon Test'  # Name of the Beacon service
 api_version = 'v2.0.0'  # Version of the Beacon implementation
-uri = 'https://beacon-apis-test.ega-archive.org/api'
+uri = 'https://beacon-apis-test.ega-archive.org/api/'
 
 #
 # Beacon granularity
