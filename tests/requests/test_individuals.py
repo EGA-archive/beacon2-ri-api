590f9a4341785473166b00770f0c6facf16eeffd
Oriol López-Doriga <orildsaga@gmail.com>
2024-02-07 11:11:06 +0100

    MISS responses exists false

--- tests/requests/test_individuals.py
+++ tests/requests/test_individuals.py
@@ -11,8 +11,7 @@ from beacon import conf
 
 def create_app():
     beacon = web.Application(
-        middlewares=[web.normalize_path_middleware(), middlewares.error_middleware, cors_middleware(origins=["https://beacon-network-test.ega-archive.org", "https://beacon-network-test2.ega-archive.org", "https://beacon-network-demo.ega-archive.org","https://beacon-network-demo2.ega-archive.org", "http://localhost:3000", "http://localhost:3010", "https://beacon-network-cineca-demo.ega-archive.org"])],            host=getattr(conf, "beacon_host", "0.0.0.0"),
-            port=getattr(conf, "beacon_port", 5050)
+        middlewares=[web.normalize_path_middleware(), middlewares.error_middleware, cors_middleware(origins=["https://beacon-network-test.ega-archive.org", "https://beacon-network-test2.ega-archive.org", "https://beacon-network-demo.ega-archive.org","https://beacon-network-demo2.ega-archive.org", "http://localhost:3000", "http://localhost:3010", "https://beacon-network-cineca-demo.ega-archive.org"])]
     )
     beacon.add_routes(routes)
     path=None
@@ -20,6 +19,6 @@ def create_app():
     return beacon
 
 async def test_get_individuals(aiohttp_client):
-    client = await aiohttp_client(create_app())
-    resp = await client.get("/")
+    client = await aiohttp_client(create_app(), server_kwargs={'port': 5050})
+    resp = await client.get("/api")
     assert resp.status == 200
