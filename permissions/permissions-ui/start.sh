0f821eb7f1e49aadec4823ee8cb927d1997700dd
Oriol López-Doriga <orildsaga@gmail.com>
2024-02-26 17:19:28 +0100

    fixing deployment of beacon permissions ui

--- permissions/permissions-ui/start.sh
+++ permissions/permissions-ui/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+python3 permissions/permissions-ui/manage.py reset_db
 python3 permissions/permissions-ui/manage.py migrate
 python3 permissions/permissions-ui/manage.py runserver 0.0.0.0:8000
