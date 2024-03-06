9e04a56ec6b284f65b1dc1b1efdc59668e129791
Joe Kane <jkane@fsjd.org>
2024-03-06 21:49:23 +0100

    adds admin user and pass back in
    beacon IdP returns

--- deploy/keycloak-entrypoint.sh
+++ deploy/keycloak-entrypoint.sh
@@ -5,6 +5,7 @@ echo "Keycloak"
 # cd /opt/keycloak/conf/certs
 # ./gencerts.sh
 
+#/opt/keycloak/bin/kc.sh import --file=/tmp/beacon-realm.json --optimized
 /opt/keycloak/bin/kc.sh build
 /opt/keycloak/bin/kc.sh show-config
-/opt/keycloak/bin/kc.sh start --optimized --verbose
+/opt/keycloak/bin/kc.sh start --optimized
