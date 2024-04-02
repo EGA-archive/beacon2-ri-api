9baf773890e01ef90c6684ea29cf6a02414d665a
Oriol López-Doriga <orildsaga@gmail.com>
2024-04-02 16:51:15 +0200

    extracting all kind of characters for second part of ontologies

--- beacon/db/extract_filtering_terms.py
+++ beacon/db/extract_filtering_terms.py
@@ -24,7 +24,7 @@ sys.path.append(parent)
 import conf
 
 
-ONTOLOGY_REGEX = re.compile(r"([_A-Za-z0-9]+):([_A-Za-z0-9^\-]+)")
+ONTOLOGY_REGEX = re.compile(r"([_A-Za-z0-9]+):([*]+)")
 
 client = MongoClient(
         #"mongodb://127.0.0.1:27017/"
