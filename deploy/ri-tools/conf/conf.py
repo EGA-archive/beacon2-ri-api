21119cbdfa24b016bac653b5d101a0837503b2f8
Oriol Lopez-Doriga <orildsaga@gmail.com>
2024-08-30 19:19:40 +0200

    adding database parameters to conf for ri-tools image

--- deploy/ri-tools/conf/conf.py
+++ deploy/ri-tools/conf/conf.py
@@ -1,7 +1,15 @@
 #### Input and Output files config parameters ####
-csv_folder = './csv/examples/'
+csv_folder = './csv/DATOS-CAT/'
 output_docs_folder='./output_docs/'
 
 #### VCF Conversion config parameters ####
 allele_frequency=1 # introduce float number, leave 1 if you want to convert all the variants
 reference_genome='GRCh38' # Choose one between NCBI36, GRCh37, GRCh38
+
+### MongoDB parameters ###
+database_host = 'mongo'
+database_port = 27017
+database_user = 'root'
+database_password = 'example'
+database_name = 'beacon'
+database_auth_source = 'admin'
